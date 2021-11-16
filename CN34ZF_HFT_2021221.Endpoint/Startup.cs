7c6fe3e95fa7ea41c49f4aa8bf341fe4318b6a2e
Nelson1313 <nelson13@stud.uni-obuda.hu>
2021-11-16 23:16:53 +0100

    Add ConfigureServices

--- CN34ZF_HFT_2021221.Endpoint/Startup.cs
+++ CN34ZF_HFT_2021221.Endpoint/Startup.cs
@@ -1,3 +1,7 @@
+using CN34ZF_HFT_2021221.Data;
+using CN34ZF_HFT_2021221.Logic;
+using CN34ZF_HFT_2021221.Repository;
+using Castle.Core.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,10 +16,21 @@ namespace CN34ZF_HFT_2021221.Endpoint
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddTransient<ITeamLogic, TeamLogic>();
+            services.AddTransient<ITeamRepository, TeamRepository>();
+            services.AddTransient<TeamsDbContext, TeamsDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
