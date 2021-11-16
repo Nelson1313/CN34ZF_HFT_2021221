6d96853cb28068627c2105c1fec3bf1f2afa749a
Nelson1313 <nelson13@stud.uni-obuda.hu>
2021-11-16 23:21:26 +0100

    Initialization

--- CN34ZF_HFT_2021221.Client/RestService.cs
+++ CN34ZF_HFT_2021221.Client/RestService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,29 @@ namespace CN34ZF_HFT_2021221.Client
 {
     class RestService
     {
+        HttpClient client;
+
+        public RestService(string baseurl)
+        {
+            
+        }
+
+        private void Init(string baseurl)
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri(baseurl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue
+                ("application/json"));
+            try
+            {
+                client.GetAsync("").GetAwaiter().GetResult();
+            }
+            catch (HttpRequestException)
+            {
+                throw new ArgumentException("Endpoint is not available!");
+            }
+        }
     }
 }
