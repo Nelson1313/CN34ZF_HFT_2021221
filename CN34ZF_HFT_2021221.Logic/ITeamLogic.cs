023be088c03333f21ea63d11bfc0f1b93ef21059
Nelson1313 <nelson13@stud.uni-obuda.hu>
2021-11-16 23:12:45 +0100

    ITeamLogic CRUD methods added

--- CN34ZF_HFT_2021221.Logic/ITeamLogic.cs
+++ CN34ZF_HFT_2021221.Logic/ITeamLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CN34ZF_HFT_2021221.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +13,10 @@ namespace CN34ZF_HFT_2021221.Logic
 
         IEnumerable<KeyValuePair<string, double>>
             AverageFoundationByTeam();
+
+        void Create(Team team);
+        IQueryable<Team> ReadAll();
+        void Update(Team team);
+        void Delete(int teamId);
     }
 }
