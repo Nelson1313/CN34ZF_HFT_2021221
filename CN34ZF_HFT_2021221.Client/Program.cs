8d85b6eb35023f8449f2d0a2368a868abdfb19d5
Nelson1313 <nelson13@stud.uni-obuda.hu>
2021-11-16 23:41:48 +0100

    Fix errors

--- CN34ZF_HFT_2021221.Client/Program.cs
+++ CN34ZF_HFT_2021221.Client/Program.cs
@@ -12,7 +12,7 @@ namespace CN34ZF_HFT_2021221.Client
         {
             System.Threading.Thread.Sleep(5000);
 
-            RestService rest = new RestService("http://localhost:59375");
+            RestService rest = new RestService("http://localhost:56403");
             var res1 = rest.Get<Team>("/team");
         }
     }
