18a4c3c09e51a8ebff9674b8e6bd74afcbd9f6ce
Nelson1313 <nelson13@stud.uni-obuda.hu>
2021-11-19 13:41:25 +0100

    ILeagueLogic interface created

---
+++ CN34ZF_HFT_2021221.Logic/ILeagueLogic.cs
@@ -0,0 +1,22 @@
+﻿using CN34ZF_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CN34ZF_HFT_2021221.Logic
+{
+    public interface ILeagueLogic
+    {
+        double AverageNumberofTeams();
+
+        IEnumerable<KeyValuePair<string, double>>
+            AverageNumberofTeamsByLeague();
+
+        void Create(League league);
+        IQueryable<League> ReadAll();
+        void Update(League league);
+        void Delete(int leagueId);
+    }
+}
