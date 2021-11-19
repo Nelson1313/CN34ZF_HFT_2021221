7610cdd8ea0d32a68c67e3472ca8c86b098081cb
Nelson1313 <nelson13@stud.uni-obuda.hu>
2021-11-19 13:52:36 +0100

    CountryController & LeagueController classes created

---
+++ CN34ZF_HFT_2021221.Endpoint/Controllers/CountryController.cs
@@ -0,0 +1,58 @@
+﻿using CN34ZF_HFT_2021221.Logic;
+using CN34ZF_HFT_2021221.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CN34ZF_HFT_2021221.Logic
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private ICountryLogic logic;
+
+        public CountryController(ICountryLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet("test")]
+        public string Test()
+        {
+
+            return "TESTEST";
+        }
+
+        [HttpGet]
+        public IEnumerable<Country> GetAll()
+        {
+
+            return logic.ReadAll();
+        }
+
+        [HttpPost]
+        public void CreateOne([FromBody] Country country)
+        {
+
+            logic.Create(country);
+        }
+
+        [HttpPut]
+        public void UpdateOne([FromBody] Country country)
+        {
+
+            logic.Update(country);
+        }
+
+        [HttpDelete("{countryId}")]
+        public void DeleteOne([FromRoute] int countryId)
+        {
+
+            logic.Delete(countryId);
+        }
+    }
+}
---
+++ CN34ZF_HFT_2021221.Endpoint/Controllers/LeagueController.cs
@@ -0,0 +1,58 @@
+﻿using CN34ZF_HFT_2021221.Logic;
+using CN34ZF_HFT_2021221.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CN34ZF_HFT_2021221.Logic
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LeagueController : ControllerBase
+    {
+        private ILeagueLogic logic;
+
+        public LeagueController(ILeagueLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet("test")]
+        public string Test()
+        {
+
+            return "TESTEST";
+        }
+
+        [HttpGet]
+        public IEnumerable<League> GetAll()
+        {
+
+            return logic.ReadAll();
+        }
+
+        [HttpPost]
+        public void CreateOne([FromBody] League league)
+        {
+
+            logic.Create(league);
+        }
+
+        [HttpPut]
+        public void UpdateOne([FromBody] League league)
+        {
+
+            logic.Update(league);
+        }
+
+        [HttpDelete("{leagueId}")]
+        public void DeleteOne([FromRoute] int leagueId)
+        {
+
+            logic.Delete(leagueId);
+        }
+    }
+}
